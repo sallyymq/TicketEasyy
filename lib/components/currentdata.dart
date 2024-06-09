a6b8d2ae4a4eb66ed652ca0c8c268e082aa09c60
Raghad Abo Ajamia <118125828+RaghadAjamia@users.noreply.github.com>
2024-06-09 21:36:05 +0300

    Update currentdata.dart

--- lib/components/currentdata.dart
+++ lib/components/currentdata.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ticketeasy/models/cards.dart';
 import 'package:ticketeasy/theme/colors.dart';
 
-class CurrentData extends StatelessWidget {
+class CurrentData extends StatefulWidget {
   final CCard card;
   final TextEditingController complexController;
   const CurrentData({Key? key, required this.card, required this.complexController}) : super(key: key);
 
   @override
+  _CurrentDataState createState() => _CurrentDataState();
+}
+
+class _CurrentDataState extends State<CurrentData> {
+  String? _errorText;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.complexController.text = widget.card.Complex;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    complexController.text = card.Complex; // Pre-fill with existing value
     return Container(
       height: 80,
       decoration: BoxDecoration(
@@ -45,14 +58,51 @@ class CurrentData extends StatelessWidget {
           SizedBox(width: 60),
           Expanded(
             child: TextField(
-              controller: complexController,
+              controller: widget.complexController,
+              inputFormatters: [ComplexInputFormatter()],
               decoration: InputDecoration(
                 hintText: 'Enter complex',
+                errorText: _errorText,
               ),
+              onChanged: (text) {
+                setState(() {
+                  _errorText = null; 
+                });
+              },
+              onEditingComplete: () {
+                final text = widget.complexController.text;
+                if (!_isValidComplex(text)) {
+                  setState(() {
+                    _errorText = 'Invalid input';
+                  });
+                } else {
+                  setState(() {
+                    _errorText = null;
+                  });
+                }
+              },
             ),
           ),
         ],
       ),
     );
+  }
+
+  bool _isValidComplex(String text) {
+    final RegExp regExp = RegExp(r'^(JUST|Amman|on way|-)$');
+    return regExp.hasMatch(text);
+  }
+}
+
+class ComplexInputFormatter extends TextInputFormatter {
+  final RegExp _partialRegExp = RegExp(r'^(JUST|Amman|on way|-|J|JU|JUS|A|Am|Amm|Amma|Amman|o|on|on |on w|on wa|on way|-)?$');
+
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    if (_partialRegExp.hasMatch(newValue.text)) {
+      return newValue;
+    } else {
+      return oldValue;
+    }
   }
 }
